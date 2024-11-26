660bc7a6286faf7c9b25aac2bbe48ea5bb4de25d
Willy Bido <willybido28@gmail.com>
2024-11-26 22:04:51 +0000

    more stuff

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -3,6 +3,20 @@ return {
   {
     -- Powerful Git integration for Vim
     'tpope/vim-fugitive',
+    config = function()
+      -- Git Status
+      vim.api.nvim_set_keymap('n', '<leader>gs', ':Git<CR>', { noremap = true, silent = true })
+      -- Git Commit
+      vim.api.nvim_set_keymap('n', '<leader>gc', ':Git commit -m ""', { noremap = true, silent = true })
+      -- Git Push
+      vim.api.nvim_set_keymap('n', '<leader>gp', ':Git push<CR>', { noremap = true, silent = true })
+
+      -- Git Pull
+      vim.api.nvim_set_keymap('n', '<leader>gl', ':Git pull --rebase<CR>', { noremap = true, silent = true })
+
+      -- Git Fetch
+      vim.api.nvim_set_keymap('n', '<leader>gf', ':Git fetch<CR>', { noremap = true, silent = true })
+    end,
   },
   {
     -- GitHub integration for vim-fugitive
